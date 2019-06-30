d035ab3e71ed238c6c0f1697350160565d265013
kowama <n.kowama@gmail.com>
2019-06-30 21:25:40 +0100

    Add setup pages

--- pages/views.py
+++ pages/views.py
@@ -7,3 +7,15 @@ def index(request):
 
 def about(request):
     return render(request, 'pages/about.html')
+
+
+def listings(request):
+    return render(request, 'pages/about.html')
+
+
+def register(request):
+    return render(request, 'pages/about.html')
+
+
+def login(request):
+    return render(request, 'pages/about.html')
