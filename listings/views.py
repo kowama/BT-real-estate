64d1c0482656f83dfb54b9361a15a9836cd2a698
kowama <n.kowama@gmail.com>
2019-07-01 23:16:51 +0100

    Add search functionality implementation

--- listings/views.py
+++ listings/views.py
@@ -1,12 +1,13 @@
 from django.core.paginator import Paginator
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Listing
+from .choices import state_choices, bedroom_choices, price_choices
 
 
 def index(request):
     listings_all = Listing.objects.order_by('-list_day').filter(is_published=True)
-    paginator = Paginator(listings_all, 12)
+    paginator = Paginator(listings_all, 6)
     page = request.GET.get('page')
     page_listings = paginator.get_page(page)
 
@@ -17,7 +18,7 @@ def index(request):
 
 
 def listing(request, listing_id):
-    item = Listing.objects.get(id=listing_id)
+    item = get_object_or_404(Listing, pk=listing_id)
     context = {
         'listing': item
     }
@@ -25,4 +26,39 @@ def listing(request, listing_id):
 
 
 def search(request):
-    return render(request, 'listings/search.html')
+    listings = Listing.objects.order_by('-list_day')
+    # search params
+    if 'keyword' in request.GET:
+        keyword = request.GET['keywords']
+        if keyword:
+            listings = listings.filter(description__icontains=keyword)
+
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            listings = listings.filter(city__icontains=city)
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            listings = listings.filter(state__icontains=state)
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            listings = listings.filter(bedrooms__exact=bedrooms)
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            listings = listings.filter(price__exact=price)
+
+    paginator = Paginator(listings, 6)
+    page = request.GET.get('page')
+    page_listings = paginator.get_page(page)
+
+    context = {
+        'state_choices': state_choices,
+        'bedrooms_choices': bedroom_choices,
+        'price_choices': price_choices,
+        'search_result_page': page_listings,
+        'params': request.GET
+    }
+    return render(request, 'listings/search.html', context)
--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render
+from listings.models import Listing
+from realtors.models import Realtor
+from listings.choices import state_choices, bedroom_choices, price_choices
 
 
 def index(request):
-    return render(request, 'pages/index.html')
+    listings = Listing.objects.order_by('-list_day').filter(is_published=True)[:3]
+    context = {
+        'state_choices': state_choices,
+        'bedrooms_choices': bedroom_choices,
+        'price_choices': price_choices,
+        'latest_listings': listings
+    }
+    return render(request, 'pages/index.html', context)
 
 
 def about(request):
-    return render(request, 'pages/about.html')
+    realtors = Realtor.objects.order_by('-hire_date')
+    seller_of_the_month = realtors.filter(is_mvp=True).first()
+    context = {
+        'realtors': realtors,
+        'seller_of_the_month': seller_of_the_month
+    }
+    return render(request, 'pages/about.html', context)
 
 
 def register(request):
